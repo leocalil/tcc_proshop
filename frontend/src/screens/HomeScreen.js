8d671c03af55117f7408e2cea67f9f4d75f54b85
leocalil@gmail.com <leocalil@gmail.com>
2022-11-05 15:39:51 -0300

    Some translations

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -31,10 +31,10 @@ const HomeScreen = ({ match }) => {
         <ProductCarousel />
       ) : (
         <Link to='/' className='btn btn-light'>
-          Go Back
+          Retornar
         </Link>
       )}
-      <h1>Latest Products</h1>
+      <h1>Produtos recentes</h1>
       {loading ? (
         <Loader />
       ) : error ? (
